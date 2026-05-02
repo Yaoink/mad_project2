c3769dd638a5b544967d56256649f2295629cc25
Nancy Quan <51468095+Yaoink@users.noreply.github.com>
2026-05-02 16:03:27 -0400

    fix:allow that user permission to view journal only

--- lib/controllers/journal_service.dart
+++ lib/controllers/journal_service.dart
@@ -1,23 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'auth_service.dart';
 import '../models/journal_model.dart';
 
 class JournalService {
+  String get userId => AuthService().currentUser?.uid ?? '';
+
   final CollectionReference _journalCollection =
       FirebaseFirestore.instance.collection('journals');
 
   Future<void> addJournalEntry(JournalModel entry) async {
     try {
-      await _journalCollection.add(entry.toMap());
+      final newEntry = JournalModel(
+        id: _journalCollection.doc().id,
+        userId: userId,
+        title: entry.title,
+        content: entry.content,
+        date: entry.date,
+        mood: entry.mood,
+      );
+
+      await _journalCollection.add(newEntry.toMap());
     } catch (e) {
       print('Error adding journal entry: $e');
     }
   }
 
   Stream<List<JournalModel>> getJournalEntries() {
-    return _journalCollection.snapshots().map((snapshot) =>
-        snapshot.docs
-            .map((doc) => JournalModel.fromMap(doc.data() as Map<String, dynamic>))
-            .toList());
+    return _journalCollection
+        .where('userId', isEqualTo: userId)
+        .snapshots()
+        .map((snapshot) =>
+          snapshot.docs
+              .map((doc) => JournalModel.fromMap(doc.data() as Map<String, dynamic>))
+              .toList());
   }
 
   Future<void> updateJournalEntry(JournalModel entry) async {
